f51295a64bb84ca9cc3f8397ce9e0ed5fc63c3f1
Hbourmaud <hugo.bourmaud@ynov.com>
2024-01-15 20:05:12 +0100

    feat: add endpoint for create/get event

--- app/index.js
+++ app/index.js
@@ -16,6 +16,32 @@ const client = redis.createClient(6379);
   await client.ping();
 })();
 
+app.get('/createEvent', async (req, res) => {
+  if (!isNaN(req.query.duration)) {
+    res.send("invalid duration argument");
+
+    return;
+  }
+
+  client.hSet(req.query.name, req.query.location, req.query.type);
+  client.expire(req.query.name, req.query.duration);
+  res.send({result: `event ${req.query.name} created`, duration: req.query.duration, location: req.query.location, type: req.query.type});
+});
+
+app.get('/event', async (req, res) => {
+  data = await client.hGetAll(req.query.eventName);
+  timeLeft = await client.ttl(req.query.eventName);
+
+if (data != null && timeLeft != -2){
+  res.send({data, timeLeft});
+
+  return;
+}
+
+res.send(JSON.stringify("No event corresponding"));
+
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
