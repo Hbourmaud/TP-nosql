2d5cb2eced1f4f1cb7cd3ccafc20445a09ba345a
Noukho <gaetan.payen@ynov.com>
2024-01-16 08:46:18 +0100

    first commit user

---
+++ app/users.js
@@ -0,0 +1,32 @@
+
+app.get('/users', async (req, res) => {
+
+    const cacheKey = 'users';
+    const data = await client.get(cacheKey);
+    console.log(req.query.pUserId);
+    console.log(req.query.pUserId);
+    console.log(req.query.pUserId);
+    console.log(req.query.pUserId);
+    console.log(req.query.pUserId);
+
+    if(data != null){
+
+    } 
+    else {
+        client.hSet('user:1', {
+            name: pName,
+            userId: pUserId,
+            age: pAge
+    //       lastPos:
+        })
+      res.send
+}
+
+});
+
+
+function createUsers(pName, pUserId, pAge)
+{
+
+  return pUserId
+}
