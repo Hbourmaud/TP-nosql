49eb19bc80ea623b3ea552bc8e11e81aa60917ff
Hbourmaud <hugo.bourmaud@ynov.com>
2024-02-05 10:31:40 +0100

    chore: rebase on main

--- app/index.js
+++ app/index.js
@@ -7,16 +7,16 @@ const app = express();
 const client = redis.createClient(6379);
 require('./users.js')(app, client);
 
-// (async () => {
-//   client.on('error', (err) => {
-  //     console.log('Redis Client Error', err);
-//   });
-//   client.on('ready', () => console.log('Redis is ready'));
+(async () => {
+  client.on('error', (err) => {
+      console.log('Redis Client Error', err);
+  });
+  client.on('ready', () => console.log('Redis is ready'));
 
-//   await client.connect();
+  await client.connect();
 
-//   await client.ping();
-// })();
+  await client.ping();
+})();
 
 const MongoClient = require('mongoose');
 
@@ -221,47 +221,6 @@ freeItem1.save().then(() => {
   }
 );
 
-// let output;
-// (async () => {
-//   output = await player1.save();
-// })
-// console.log(output);
-
-// app.get('/createEvent', async (req, res) => {
-//   if (isNaN(req.query.duration)) {
-//     res.send("invalid duration argument");
-
-//     return;
-//   }
-
-//   client.hSet(req.query.name, req.query.location, req.query.type);
-//   client.expire(req.query.name, req.query.duration);
-//   res.send({result: `event ${req.query.name} created`, duration: req.query.duration, location: req.query.location, type: req.query.type});
-// });
-
-// app.get('/event', async (req, res) => {
-//   if(!req.query.eventName){
-//     keys = await client.keys('*');
-//   } else {
-//     keys = [req.query.eventName];
-//   }
-
-//   eventsRes = [];
-
-//   eventsRes = Promise.all(keys.map(async key => {
-//     data = await client.hGetAll(key);
-//     timeLeft = await client.ttl(key);
-//     if (data != null && timeLeft != -2){
-//       return ({'name': key, data, timeLeft});
-//     }
-//   }));
-
-//   res.send(await eventsRes);
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
 app.get('/errors', (req, res) =>
 {
   userId = CleanParameter(req.query);
